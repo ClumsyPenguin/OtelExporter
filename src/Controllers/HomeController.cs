50ae91f8cddb668ca21d50052431e4b47849bd4c
Seppe Geerinckx <seppe.geerinckx@bmileisure.com>
2023-10-26 15:42:24 +0200

    quick mvp for github issue

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace OtelExporter.Controllers
+{
+    [RoutePrefix("home")]
+    public class HomeController : ApiController
+    {
+        [HttpGet, Route("")]
+        public IHttpActionResult Test(CancellationToken cancellationToken = default)
+            => Ok("Authenticated");
+    }
+}
---
+++ src/Global.asax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace OtelExporter
+{
+    public class MvcApplication : HttpApplication
+    {
+        private TracerProvider _tracerProvider;
+        
+        protected void Application_Start()
+        {
+            _tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddAspNetInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddSqlClientInstrumentation()
+                .AddOtlpExporter(options =>
+                    {
+                        options.Endpoint = new Uri($"http://localhost:4317");
+                    })
+                .AddSource("my-service-name")
+                .SetResourceBuilder(
+                    ResourceBuilder.CreateDefault()
+                        .AddService(serviceName: "my-service-name", serviceVersion: "1.0.0"))
+
+                .Build();
+        }
+        
+        protected void Application_End()
+        {
+            _tracerProvider?.Dispose();
+        }
+    }
+}
